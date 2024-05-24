7e311a2af98bba2ee29be94d2a5c1e274d729171
tricky <trickya23@gmail.com>
2024-05-24 09:04:04 +0000

    fix changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,10 +4,6 @@ import { inter } from "@/components/fonts";
 import Header from "@/components/Header";
 import "./globals.css";
 import Footer from "@/components/Footer";
-import Header from "@/components/Header";
-import { ThemeProvider } from "@/providers/themeProvider";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Dr. Ambrose Togobo",

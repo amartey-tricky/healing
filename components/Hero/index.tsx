f63bc1e63e255896960b51d8f0d53e089f70660e
tricky <trickya23@gmail.com>
2024-05-28 08:16:29 +0000

    add hero section

--- components/Hero/index.tsx
+++ components/Hero/index.tsx
@@ -1,9 +1,20 @@
-import styles from "./index.module.css"
+import styles from "./index.module.css";
+import Link from "next/link";
+import Image from "next/image";
 
 export default function Home() {
   return (
     <section className={styles.hero}>
-      Hero
+      <div className={styles.heroContainer}>
+        <div>
+          <h1 className={styles.heading}>Dr Ambrose Kofi Togobo</h1>
+          <p className={styles.p}>Dr Togobo is a prolific author and medical doctor. He is a member of the Royal College of Anaesthetists, who has written over 3 books. He has a knack for delivering a great experience to the reader when they least expect it.</p>
+          <Link href="/books" className={styles.link}>Discover Books</Link>
+        </div>
+        <div className={styles.image}>
+          <Image src="" alt="image of Dr Togobo" width={350} height={400} />
+        </div>
+      </div>
     </section>
   )
 }

0e6f65329c656af197ee97b500decf80fd520405
tricky <trickya23@gmail.com>
2024-05-28 13:13:10 +0000

    update hero section

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,9 @@
 import Hero from "@/components/Hero"
+import styles from "./index.module.css"
 
 export default function Home() {
   return (
-    <main>
+    <main className={styles.main}>
       <Hero />
     </main>
   );
--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,16 @@
+const appId = process.env.UPLOADTHING_APP_ID
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "utfs.io",
+        // pathname: "/a/appId/*"
+      }
+    ]
+  }
 };
 
 export default nextConfig;
